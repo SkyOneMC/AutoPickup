7bc654102fd5e582152ddd177c5e1a92aaba5633
RV_SkeLe <rvskele@gmail.com>
2025-06-16 11:52:25 +0200

    Clean hooks

--- src/main/java/us/thezircon/play/autopickup/utils/PluginHooks.java
+++ src/main/java/us/thezircon/play/autopickup/utils/PluginHooks.java
@@ -6,22 +6,14 @@ import org.bukkit.Bukkit;
 @Getter
 public class PluginHooks {
 
-    private final boolean usingUpgradableHoppers;
-    private final boolean usingLocketteProByBrunyman;
-    private final boolean usingQuickShop;
-    private final boolean usingEpicFurnaces;
-    private final boolean usingWildChests;
     private final boolean usingMythicMobs;
     private final boolean usingPlaceholderAPI;
+    private final boolean usingWildStacker;
 
     public PluginHooks() {
-        this.usingUpgradableHoppers = isPluginEnabled("UpgradeableHoppers");
-        this.usingLocketteProByBrunyman = isPluginEnabled("LockettePro");
-        this.usingQuickShop = isPluginEnabled("QuickShop");
-        this.usingEpicFurnaces = isPluginEnabled("EpicFurnaces");
-        this.usingWildChests = isPluginEnabled("WildChests");
         this.usingMythicMobs = isPluginEnabled("MythicMobs");
         this.usingPlaceholderAPI = isPluginEnabled("PlaceholderAPI");
+        this.usingWildStacker = isPluginEnabled("WildStacker");
     }
 
     private boolean isPluginEnabled(String pluginName) {
